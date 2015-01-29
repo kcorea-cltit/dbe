108f7f2135e5a7f2b2cdba430e49b63345d4df39
Kenneth Corea <kcorea@hawaii.edu>
2015-01-28 14:17:29 -1000

    fixed shared; hadn't downloaded raw file derp

--- dbe/shared/templatetags/dbetags.py
+++ dbe/shared/templatetags/dbetags.py
@@ -1,1 +1,24 @@
-<html><body>You are being <a href="https://github.com/akulakov/django/blob/master/dbe/shared/templatetags/dbetags.py">redirected</a>.</body></html>
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+def getattribute(value, arg):
+    """Gets an attribute of an object dynamically from a string name"""
+    if hasattr(value, str(arg)):
+        return getattr(value, arg)
+    elif hasattr(value, 'has_key') and value.has_key(arg):
+        return value[arg]
+    else:
+        return settings.TEMPLATE_STRING_IF_INVALID
+
+def get(value, arg):
+    return value[arg]
+
+def concat(value, arg):
+    return value + arg
+
+
+register.filter("getattribute", getattribute)
+register.filter("get", get)
+register.filter("concat", concat)
